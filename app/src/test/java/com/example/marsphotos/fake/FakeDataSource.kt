bf76f288b0ea7daf20f31b53de1e89fd6c597985
Augusto L <juanaugustolombino@protonmail.ch>
2024-05-29 05:43:05 -0300

    Upload project

---
+++ app/src/test/java/com/example/marsphotos/fake/FakeDataSource.kt
@@ -0,0 +1,21 @@
+package com.example.marsphotos.fake
+
+import com.example.marsphotos.network.MarsPhoto
+
+object FakeDataSource {
+    private const val idOne = "img1"
+    private const val idTwo = "img2"
+    private const val imgOne = "url.1"
+    private const val imgTwo = "url.2"
+
+    val photosList: List<MarsPhoto> = listOf(
+        MarsPhoto(
+            id = idOne,
+            imgSrc = imgOne
+        ),
+        MarsPhoto(
+            id = idTwo,
+            imgSrc = imgTwo
+        )
+    )
+}
